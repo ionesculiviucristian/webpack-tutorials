fe52a30beb68a99b577082c9445babdc7b39ea6a
Ionescu Liviu Cristian <ionescu.liviucristian@yahoo.com>
2019-10-05 16:26:14 +0300

    Add dev/prod configs. Add live server.

--- freeCodeCamp.org - Learn Webpack - Full Tutorial for Beginners/webpack.common.js
+++ freeCodeCamp.org - Learn Webpack - Full Tutorial for Beginners/webpack.common.js
@@ -2,22 +2,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    //devtool: "none",
     entry: "./src/index.js",
-    output: {
-        filename: "main.[contentHash]js",
-        path: path.resolve(__dirname, "dist")
-    },
     plugins: [new HtmlWebpackPlugin({
         template: "./src/template.html"
     })],
     module: {
         rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
             {
                 test: /\.scss$/i,
                 use: [
