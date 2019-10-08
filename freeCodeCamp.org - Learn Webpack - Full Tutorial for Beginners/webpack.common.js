1dc5c2384393e60bfe2a4348e15793b70518c7b7
Ionescu Liviu Cristian <ionescu.liviucristian@yahoo.com>
2019-10-08 17:55:15 +0300

    Add minification.

--- freeCodeCamp.org - Learn Webpack - Full Tutorial for Beginners/webpack.common.js
+++ freeCodeCamp.org - Learn Webpack - Full Tutorial for Beginners/webpack.common.js
@@ -1,24 +1,10 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 module.exports = {
     entry: {
         main: "./src/index.js",
         vendor: "./src/vendor.js"
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/template.html"
-    })],
     module: {
         rules: [
-            {
-                test: /\.scss$/i,
-                use: [
-                    "style-loader", // 3. inject styles into DOM
-                    "css-loader",   // 2. turns CSS into CommonJS
-                    "sass-loader"   // 1. turns SASS into CSS
-                ]
-            },
             {
                 test: /\.html$/i,
                 use: ["html-loader"]
@@ -33,6 +19,6 @@ module.exports = {
                     }
                 }
             }
-        ],
+        ]
     }
 };
--- freeCodeCamp.org - Learn Webpack - Full Tutorial for Beginners/webpack.prod.js
+++ freeCodeCamp.org - Learn Webpack - Full Tutorial for Beginners/webpack.prod.js
@@ -2,6 +2,10 @@ const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
@@ -9,7 +13,34 @@ module.exports = merge(common, {
         filename: "[name].[contentHash].bundle.js",
         path: path.resolve(__dirname, "dist")
     },
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserPlugin(),
+            new HtmlWebpackPlugin({
+                template: "./src/template.html",
+                minify: {
+                    removeAttributeQuotes: true,
+                    collapseWhitespace: true,
+                    removeComments: true
+                }
+            })
+        ]
+    },
     plugins: [
+        new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
         new CleanWebpackPlugin()
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, // 3. extract CSS into files
+                    "css-loader",                // 2. turns CSS into CommonJS
+                    "sass-loader"                // 1. turns SASS into CSS
+                ]
+            }
+        ]
+    }
 });
